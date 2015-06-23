a38dd476da45376f1e6fae03a50256f5b1cdf580
Joe Flasher <joseph.flasher@gmail.com>
2015-06-23 14:15:56 -0400

    Initial commit of view-latest example

---
+++ README.md
@@ -0,0 +1,12 @@
+# Most Recently Published Viewer
+
+## Overview
+
+The code contained in this repository is an example of how you would go about building a webapp to display the most recently published imagery from the Astro Digital platform and displaying some associated metadata.
+
+This example illustrates interacting with several Astro Digital API endpoints and how to use the returned data.
+
+## Running
+
+The webapp is written in only HTML, CSS and JS and does not require the installation of any special frameworks. As such, you can run it with any static server setup you want, see [here](https://gist.github.com/willurd/5720255) for a whole bunch of examples.
+
---
+++ js/main.js
@@ -0,0 +1,279 @@
+/* global XMLHttpRequest, L */
+'use strict';
+
+/**
+ * How many recently published images to display?
+ */
+var scenesToFetch = 10;
+
+/**
+ * A valid Mapbox token.
+ */
+var accessToken = 'pk.eyJ1IjoiYXN0cm9kaWdpdGFsIiwiYSI6ImNVb1B0ZkEifQ.IrJoULY2VMSBNFqHLrFYew';
+
+/**
+ * How long to wait between switching published images? 30 seconds seems good.
+ */
+var sceneSwitchTime = 30 * 1000;
+
+/**
+ * How often to refresh the page to get new imagery? Every 2 hours seems
+ * appropriate.
+ */
+var scenesRefreshTime = 2 * 60 * 60 * 1000;
+
+/**
+ * The API base URL
+ */
+var baseURL = 'https://api.astrodigital.com/v1/';
+
+/**
+ * The Mapbox URL format for tiled images.
+ */
+var mbUrl = 'https://{s}.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={token}';
+
+/**
+ * These are all global variables that don't need editing.
+ */
+var map;
+var currentScene;
+var currentScenes = [];
+var sceneIndex = 0;
+var methods;
+var satellites;
+
+/**
+ * Calls out to the API to get bounds and center for a given scene and adds
+ *  it to the scene object.
+ * @param {object} scene - a valid scene object to get bounds for.
+ * @param {function} cb - A callback to denote completion.
+ */
+var getSceneBounds = function (scene, cb) {
+  // If we already have bounds on the scene, we do not need to call out to the
+  // API again.
+  if (scene.bounds) {
+    return cb();
+  }
+
+  // Request bounds information from the API
+  var request = new XMLHttpRequest();
+  request.onload = function () {
+    var data = JSON.parse(this.responseText).results[0];
+    var sw = [data.lowerLeftCornerLatitude, data.lowerLeftCornerLongitude];
+    var ne = [data.upperRightCornerLatitude, data.upperRightCornerLongitude];
+    var bounds = L.latLngBounds(sw, ne);
+    scene.bounds = bounds;
+    scene.sceneCenterLongitude = data.sceneCenterLongitude;
+    scene.sceneCenterLatitude = data.sceneCenterLatitude;
+    cb();
+  };
+  var searchURL = baseURL + 'search?search=' + scene.scene_id;
+  request.open('GET', searchURL, true);
+  request.send();
+};
+
+/**
+ * Calls out to the API to get the latest published scenes, making multiple
+ * requests if needed to ensure it gets enough images.
+ * @param {function} cb - A callback with response cb(scenes)
+ */
+var getLatestScenes = function (cb) {
+  var scenes = [];
+
+  // Get list of scenes from the API
+  var getScenes = function (url, cb) {
+    var request = new XMLHttpRequest();
+    request.onload = function () {
+      var data = JSON.parse(this.responseText);
+      cb(data.results, data.previous);
+    };
+    request.open('GET', url, true);
+    request.send();
+  };
+
+  // Add received scenes to the total and see if we have enough. If we don't,
+  // keep on requesting more. If we do, do some data formating to turn it into
+  // what we want for display.
+  var handleAPIResponse = function (data, previous) {
+    // Add new scenes to previous array.
+    scenes.push.apply(scenes, data);
+
+    // Check to see if we have enough results, if not, keep
+    // loading previous pages to get to the number we want.
+    var numberOfScenes = scenes.length;
+    if (numberOfScenes < scenesToFetch) {
+      // Moar scenes!
+      getScenes(previous, handleAPIResponse);
+    } else {
+      // We have enough images, now sort them in reverse chronological order
+      scenes.sort(function compare (a, b) {
+        if (a.time_requested > b.time_requested) {
+          return -1;
+        }
+        if (a.time_requested < b.time_requested) {
+          return 1;
+        }
+        return 0;
+      });
+
+      // Remove any scenes that aren't yet ready
+      var readyScenes = [];
+      for (var i = 0; i < scenes.length; i++) {
+        if (scenes[i].ready) {
+          readyScenes.push(scenes[i]);
+        }
+      }
+      scenes = readyScenes;
+
+      // And trim to total number we want
+      scenes = scenes.splice(0, scenesToFetch);
+      if (cb && typeof cb === 'function') {
+        cb(scenes);
+      }
+    }
+  };
+
+  var scenesURL = baseURL + 'scenes?page=last';
+  getScenes(scenesURL, handleAPIResponse);
+};
+
+/**
+ * Adds a new layer to the map, moves to it and removes previous layer.
+ * @param {object} scene - a valid scene object to display.
+ */
+var displayScene = function (scene) {
+  // Add new scene to the map
+  var newScene = L.tileLayer(mbUrl, {id: scene.map_id, token: accessToken});
+  newScene.addTo(map);
+  map.fitBounds(scene.bounds, { animate: true, padding: [50, 50] });
+
+  // Set info box content
+  setInfo(scene);
+
+  // Remove old layer
+  if (map.hasLayer(currentScene)) {
+    map.removeLayer(currentScene);
+  }
+
+  // Update current scene
+  currentScene = newScene;
+};
+
+/**
+ * Gets list of methods supported by the Astro Digital platform.
+ * @param {function} callback - A callback to denote completion.
+ */
+var getMethods = function (cb) {
+  var methodsURL = baseURL + 'methods';
+  var request = new XMLHttpRequest();
+  request.onload = function () {
+    var data = JSON.parse(this.responseText);
+    methods = data.results;
+    cb();
+  };
+  request.open('GET', methodsURL, true);
+  request.send();
+};
+
+/**
+ * A simple helper to get the method object by its id.
+ * @param {int} id - a valid method id.
+ */
+var getMethodByID = function (id) {
+  for (var i = 0; i < methods.length; i++) {
+    if (methods[i].id === id) {
+      return methods[i];
+    }
+  }
+};
+
+/**
+ * Gets list of satellites supported by the Astro Digital platform.
+ * @param {function} callback - A callback to denote completion.
+ */
+var getSatellites = function (cb) {
+  var satellitesURL = baseURL + 'satellites';
+  var request = new XMLHttpRequest();
+  request.onload = function () {
+    var data = JSON.parse(this.responseText);
+    satellites = data.results;
+    cb();
+  };
+  request.open('GET', satellitesURL, true);
+  request.send();
+};
+
+/**
+ * A simple helper to get the satellite object by its id.
+ * @param {int} id - a valid satellite id.
+ */
+var getSatelliteByID = function (id) {
+  for (var i = 0; i < satellites.length; i++) {
+    if (satellites[i].id === id) {
+      return satellites[i];
+    }
+  }
+};
+
+/**
+ * Update the information in the on-screen info box.
+ * @param {object} scene - a valid scene object to display info for.
+ */
+var setInfo = function (scene) {
+  document.getElementById('location').innerHTML = scene.sceneCenterLongitude +
+    ', ' + scene.sceneCenterLatitude;
+  document.getElementById('satellite').innerHTML = getSatelliteByID(scene.satellite).name;
+  document.getElementById('method').innerHTML = getMethodByID(scene.process_method).name;
+};
+
+/**
+ * Loop over each of the recently published scenes and display it, forever...
+ */
+var playScenes = function () {
+  getSceneBounds(currentScenes[sceneIndex], function () {
+    displayScene(currentScenes[sceneIndex]);
+  });
+  setTimeout(function () {
+    sceneIndex++;
+    if (sceneIndex >= currentScenes.length) {
+      sceneIndex = 0;
+    }
+    playScenes();
+  }, sceneSwitchTime);
+};
+
+/**
+ * Initialize the map and add a baselayer.
+ */
+var initMap = function () {
+  // Create new Leaflet map and set initial view
+  map = new L.Map('map', { zoomControl: false }).setView([0, 0], 8);
+
+  // Add base layer
+  L.tileLayer(mbUrl, {id: 'astrodigital.00ffdda1', token: accessToken, minZoom: 4, maxZoom: 12}).addTo(map);
+};
+initMap();
+
+/**
+ * This is where we kick everything off. We first grab satellite and methods
+ * metadata from the API and then start grabbing the latest scenes.
+ */
+getMethods(function () {
+  getSatellites(function () {
+  // Get the latest scenes to load into view
+    getLatestScenes(function (scenes) {
+      currentScenes = scenes;
+      playScenes();
+    });
+  });
+});
+
+/**
+ * For long-term display purposes, we just have the page refresh every so often
+ * to make sure we are always getting the latest images.
+ */
+setInterval(function () {
+  getLatestScenes(function (scenes) {
+    currentScenes = scenes;
+  });
+}, scenesRefreshTime);
